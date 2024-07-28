174953f4ddf0637cd0a349b991e30e7992a75c1e
muhammetsefayasar@gmail.com <muhammetsefayasar@gmail.com>
2024-07-28 11:44:43 +0300

    .

--- NLayer.Core/DTOs/ProductUpdateDto.cs
+++ NLayer.Core/DTOs/ProductUpdateDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core.DTOs
 {
-    internal class ProductUpdateDto
+    public class ProductUpdateDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
