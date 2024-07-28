86b753ec7dd7a25701d58c56a14fb6e4df3cca79
muhammetsefayasar@gmail.com <muhammetsefayasar@gmail.com>
2024-07-28 11:33:31 +0300

    .

--- NLayer.Core/Models/Category.cs
+++ NLayer.Core/Models/Category.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core
 {
-    public class Category:BaseEntity
+    public class Category : BaseEntity
     {
         public string Name { get; set; }
 
